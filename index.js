650c436d081b68602b403c0a8b721dae4512e01a
pallab Biswas <pallab.b7@gmail.com>
2024-12-08 07:35:47 +0600

    donate noakhali complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('js connected');
+//donate now button accessing
+
+const donateNow = document.getElementById('btn-donate').addEventListener('click', function () {
+  const donationInput = parseFloat(document.getElementById('donation-input').value);
+  let donation = parseFloat(document.getElementById('donation-amount').innerText);
+  let deposit = parseFloat(document.getElementById('deposit').innerText);
+  donation = donation + donationInput;
+  deposit = deposit - donationInput;
+  const donationOutstanding = document.getElementById('donation-amount').innerText = donation.toFixed(2);
+  const depositOutstanding = document.getElementById('deposit').innerText = deposit.toFixed(2);
+
+});
