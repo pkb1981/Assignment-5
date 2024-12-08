7f5ebb2088c473f8f8f90b8970012a1b27710237
pallab Biswas <pallab.b7@gmail.com>
2024-12-08 08:22:15 +0600

    all 3 donate section complete

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-//donate now button accessing
+//donate now button noakhali
 
-const donateNow = document.getElementById('btn-donate').addEventListener('click', function () {
-  const donationInput = parseFloat(document.getElementById('donation-input').value);
-  let donation = parseFloat(document.getElementById('donation-amount').innerText);
+const donateNow1 = document.getElementById('btn-donate1').addEventListener('click', function () {
+  const donationInput = parseFloat(document.getElementById('donation-input1').value);
+  let donation = parseFloat(document.getElementById('donation-amount1').innerText);
   let deposit = parseFloat(document.getElementById('deposit').innerText);
   donation = donation + donationInput;
   deposit = deposit - donationInput;
-  const donationOutstanding = document.getElementById('donation-amount').innerText = donation.toFixed(2);
+  const donationOutstanding = document.getElementById('donation-amount1').innerText = donation.toFixed(2);
+  const depositOutstanding = document.getElementById('deposit').innerText = deposit.toFixed(2);
+
+});
+
+// donation now button feni
+
+const donateNow2 = document.getElementById('btn-donate2').addEventListener('click', function () {
+  const donationInput = parseFloat(document.getElementById('donation-input2').value);
+  let donation = parseFloat(document.getElementById('donation-amount2').innerText);
+  let deposit = parseFloat(document.getElementById('deposit').innerText);
+  donation = donation + donationInput;
+  deposit = deposit - donationInput;
+  const donationOutstanding = document.getElementById('donation-amount2').innerText = donation.toFixed(2);
+  const depositOutstanding = document.getElementById('deposit').innerText = deposit.toFixed(2);
+
+});
+
+// donation now button quota
+
+const donateNow3 = document.getElementById('btn-donate3').addEventListener('click', function () {
+  const donationInput = parseFloat(document.getElementById('donation-input3').value);
+  let donation = parseFloat(document.getElementById('donation-amount3').innerText);
+  let deposit = parseFloat(document.getElementById('deposit').innerText);
+  donation = donation + donationInput;
+  deposit = deposit - donationInput;
+  const donationOutstanding = document.getElementById('donation-amount3').innerText = donation.toFixed(2);
   const depositOutstanding = document.getElementById('deposit').innerText = deposit.toFixed(2);
 
 });
